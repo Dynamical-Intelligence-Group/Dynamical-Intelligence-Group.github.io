8eb5aa0f6d91b21cca435861b4beaca81af52b9a
william-redman <39911316+william-redman@users.noreply.github.com>
2025-12-19 20:59:10 +0000

    Deploying to gh-pages from @ Dynamical-Intelligence-Group/Dynamical-Intelligence-Group.github.io@a4c863ea94756c875f408211df3e4d191ad827ea 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,27 +23,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
-        },{id: "nav-repositories",
-          title: "repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/repositories/";
-          },
-        },{id: "nav-cv",
-          title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/cv/";
-          },
-        },{id: "nav-teaching",
-          title: "teaching",
-          description: "Materials for courses you taught. Replace this text with your description.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/teaching/";
-          },
         },{id: "nav-people",
           title: "people",
           description: "members of the lab or group",
@@ -63,16 +42,8 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather.html";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
-          description: "",
-          section: "News",},{id: "news-a-long-announcement-with-details",
-          title: 'A long announcement with details',
-          description: "",
-          section: "News",handler: () => {
-              window.location.href = "/news/announcement_2.html";
-            },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+            },},{id: "news-new-preprint-out-on-grid-cell-computation-distance-by-de-correlation-computing-distance-with-heterogeneous-grid-cells-a-great-collaboration-with-priti-and-akshunna",
+          title: 'New preprint out on grid cell computation - Distance by de-correlation: Computing distance...',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
